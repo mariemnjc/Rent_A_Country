7955402915077ba7b40686d6b8241da2f57ff64d
Valentin Malemanche <vmalemanche@gmail.com>
2025-03-11 15:37:04 +0100

    formulaire

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,10 @@
 class BookingsController < ApplicationController
   def index
+    countries = Country.where(user_id: current_user.id)
+    @bookings = Booking.where(country: countries)
   end
+
+  # à mettre dans def index admin ?
 
   def show
   end
--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -6,14 +6,27 @@ class CountriesController < ApplicationController
   end
 
   def new
+    @country = Country.new
   end
 
   def create
+    @country = Country.new(country_params)
+    if @country.save
+      redirect_to country_path(@country)
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  def country_params
+    params(:country).permit(:name, :user_id, :continent, :language, :climate, :description, :resources)
   end
 end
